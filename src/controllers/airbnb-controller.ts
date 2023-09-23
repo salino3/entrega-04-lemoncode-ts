2de5261ab73db3594b8af3fafab38103978729c4
salino3 <programacionflavio@gmail.com>
2023-09-23 17:47:29 +0200

    implemented first mapper

--- src/controllers/airbnb-controller.ts
+++ src/controllers/airbnb-controller.ts
@@ -2,40 +2,34 @@ import { Request, Response } from "express";
 import ListingModel from "../models/listing-schema";
 import { IListingAndReview } from "../interfaces/houses";
 import { v4 as uuidv4 } from "uuid";
+import { mapAirbnbFromApiToModel } from "../mappers/airbnb.mappers";
 
 export const getAllLimitFive = async (req: Request, res: Response) => {
   try {
-    const properties: IListingAndReview[] = await ListingModel.find()
-      .limit(5)
-      .select({
-        title: 1,
-        images: 1,
-        description: 1,
-        address: 1,
-        bedrooms: 1,
-        beds: 1,
-        bathrooms: 1,
-        reviews: 1,
-      });
-         const propertiesWithLastFiveReviews = properties.map((property) => ({
-           ...property.toObject(),
-           reviews: property.reviews.slice(-5), 
-         })); 
+    const properties: IListingAndReview[] = await ListingModel.find().limit(5);
 
-    return res.json(propertiesWithLastFiveReviews);
+    const propertiesWithLastFiveReviews = properties.map((property) => ({
+      ...property.toObject(),
+      reviews: property.reviews.slice(-5),
+    }));
+
+    const mappedProperties = propertiesWithLastFiveReviews.map((property) =>
+      mapAirbnbFromApiToModel(property)
+    );
+
+    return res.json(mappedProperties);
   } catch (error) {
     console.error("Error getting data:", error);
     return res.status(500).json({ error: "Error getting data" });
   };
 };
 
-
 export const getHouseDetail = async (req: Request, res: Response) => {
   const propertyId = req.params.id;
 
   try {
     const property = await ListingModel.findById(propertyId).select({
-      title: 1,
+      name: 1,
       images: 1,
       description: 1,
       address: 1,
@@ -47,20 +41,19 @@ export const getHouseDetail = async (req: Request, res: Response) => {
 
     if (!property) {
       return res.status(404).json({ error: "Property not found" });
-    };
+    }
 
-const propertyWithLastFiveReviews = {
-  ...property.toObject(),
-  reviews: property.reviews.slice(-5),
-};
+    const propertyWithLastFiveReviews = {
+      ...property.toObject(),
+      reviews: property.reviews.slice(-5),
+    };
 
     return res.json(propertyWithLastFiveReviews);
   } catch (error) {
     console.error("Error getting details property", error);
     return res.status(500).json({ error: "Error getting details property" });
-  };
+  }
 };
-
 
 export const getHousesByCountry = async (req: Request, res: Response) => {
   const country = req.params.country;
@@ -76,13 +69,12 @@ export const getHousesByCountry = async (req: Request, res: Response) => {
     return res
       .status(500)
       .json({ error: "Error getting the houses by country" });
-  };
+  }
 };
-
 
 export const createReview = async (req: Request, res: Response) => {
   const propertyId = req.params.propertyId;
-  const {reviewer_id, reviewer_name, comments } = req.body;
+  const { reviewer_id, reviewer_name, comments } = req.body;
 
   try {
     const property = await ListingModel.findById(propertyId);
