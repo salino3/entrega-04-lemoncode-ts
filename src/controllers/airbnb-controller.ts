9243d0f9800272a9cf39b5d6d80f6fbcbbe8ae29
salino3 <programacionflavio@gmail.com>
2023-09-23 17:06:17 +0200

    fix response data method GET

--- src/controllers/airbnb-controller.ts
+++ src/controllers/airbnb-controller.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from "express";
 import ListingModel from "../models/listing-schema";
+import { IListingAndReview } from "../interfaces/houses";
 import { v4 as uuidv4 } from "uuid";
 
 export const getAllLimitFive = async (req: Request, res: Response) => {
   try {
-    const properties = await ListingModel.find().limit(5);
-    return res.json(properties);
+    const properties: IListingAndReview[] = await ListingModel.find()
+      .limit(5)
+      .select({
+        title: 1,
+        images: 1,
+        description: 1,
+        address: 1,
+        bedrooms: 1,
+        beds: 1,
+        bathrooms: 1,
+        reviews: 1,
+      });
+         const propertiesWithLastFiveReviews = properties.map((property) => ({
+           ...property.toObject(),
+           reviews: property.reviews.slice(-5), 
+         })); 
+
+    return res.json(propertiesWithLastFiveReviews);
   } catch (error) {
     console.error("Error getting data:", error);
     return res.status(500).json({ error: "Error getting data" });
@@ -17,23 +34,27 @@ export const getHouseDetail = async (req: Request, res: Response) => {
   const propertyId = req.params.id;
 
   try {
-    const property = await ListingModel.findById(propertyId);
+    const property = await ListingModel.findById(propertyId).select({
+      title: 1,
+      images: 1,
+      description: 1,
+      address: 1,
+      bedrooms: 1,
+      beds: 1,
+      bathrooms: 1,
+      reviews: 1,
+    });
+
     if (!property) {
       return res.status(404).json({ error: "Property not found" });
     };
 
-     const propertyDetail = {
-       title: property.name,
-       image: property.images.picture_url,
-       description: property.description,
-       address: property.address,
-       bedrooms: property.bedrooms,
-       beds: property.beds,
-       bathrooms: property.bathrooms,
-       reviews: property.reviews
-     };
+const propertyWithLastFiveReviews = {
+  ...property.toObject(),
+  reviews: property.reviews.slice(-5),
+};
 
-    return res.json(propertyDetail);
+    return res.json(propertyWithLastFiveReviews);
   } catch (error) {
     console.error("Error getting details property", error);
     return res.status(500).json({ error: "Error getting details property" });
